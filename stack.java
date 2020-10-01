452b90344227dc6ddf736c204e58aebe41adc504
Kuenga420 <tsheringkuengatenzin@gmail.com>
2020-10-01 12:17:25 +0600

    stack.java

--- stack.java
+++ stack.java
@@ -1,41 +1,54 @@
  class stack {
-  public static int SIZE = 10;
+  public static int limit_size = 10;
   public static int top = 0;
+   public static int size = 0;
 
-  public static int[] S = new int[SIZE+1];
+  public static int[] s = new int[limit_size];
 
   public static boolean isEmpty() {
-    if(top == 0)
+    if(size == 0)
       return true;
     return false;
   }
 
   public static int top(){
-  	return top;
-  }
+    if (top==-1){
+      return 0;
+    }
+
+      return s[top];
+    }
 
 
   public static void push(int x) {
-    top = top+1;
-    if(top > SIZE)
-      System.out.println("Stackoverflow");
-    else
-      S[top] = x;
+    if(size==0){
+      s[0]=x;
+      top=0;
+    }
+    else{
+      s[top+1]=x;
+      top++;
+
+    }
+    size=size+1;
   }
 
   public static int pop() {
-    if(isEmpty()) {
-      System.out.println("Stackunderflow");
-      return -1000;
+    if(size==0){
+      System.out.println("The stack is empty");
     }
-    else {
-      top = top-1;
-      return S[top+1];
+    else{
+      top=s[top-1];
+      size=size-1;
     }
+    return s[top+1];
+      
+
   }
+  
 
   public static int size(){
-  	return SIZE;
+  	return size;
   	
   }
 
@@ -51,12 +64,11 @@
     push(70);
     push(80);
     push(90);
-    push(100);
-    push(110);
+    
     
 
-    for(int i=1; i<=SIZE; i++) {
-      System.out.println(S[i]);
+    for(int i=1; i<=size; i++) {
+      System.out.println(s[i]);
     }
     System.out.println("this is the size: "+size()+" and the top: "+ top()+ isEmpty());
   }
